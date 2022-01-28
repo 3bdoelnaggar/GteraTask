c9a1d97f7d57f8b10eb2bc540a7d0543d21497d9
mahmoud-elnaggar <3bdoelnaggar@gmail.com>
2022-01-28 19:34:48 +0200

    show greeting and shopping list in home screen

--- app/src/main/java/com/gtera/gteratask/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/gtera/gteratask/presentation/home/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.gtera.gteratask.presentation.home
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.gtera.gteratask.R
 import com.gtera.gteratask.databinding.HomeFragmentBinding
 import com.gtera.gteratask.viewBinding
@@ -13,6 +14,7 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
     private val binding by viewBinding (HomeFragmentBinding::bind)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
         homeViewModel.stateLiveData.observe(viewLifecycleOwner){
             if(it!=null){
                 binding.greetingTextView.text=it.greeting
